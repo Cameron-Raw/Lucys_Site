de3c3c33dc0ca8784c514503ffd72ff7c5b785dd
Cameron Raw <cameron.raw@me.com>
2016-09-08 22:58:18 +0200

    Adding majority of styles to index.php

--- index.php
+++ index.php
@@ -5,6 +5,19 @@
 		<div class="container">
 
 			<h1 id="discover">Discover the power of flawless copy</h1>
+
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-4 col-centered" id="welcome-col">
+
+						<img id="lucy-img" src="img/lucymain.jpg">
+
+						<h2>Hi, I'm Lucy</h2>
+						<p>Thanks for visiting my site. I am a freelance copywriter, copy-editor and proof reader based in Barcelona, Spain. Please feel free to contact me to discuss my collaboration in your next project.<br>...</p>
+
+					</div>
+				</div>
+			</div>
 			
 
 
@@ -12,12 +25,7 @@
 
 
 
-				<img src="img/lucymain.jpg">
 
-				<h2>Hi, I'm Lucy</h2>
-				<p>Thanks for visiting my site. I am a freelance copywriter, copy-editor and proof reader
-				based in Barcelona, Spain. Please feel free to contact me to discuss my collaboration
-				in your next project.<br>...</p>
 
 
 			
