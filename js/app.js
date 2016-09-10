8987ab9492aef1dc7fd3dcd28f4bb322f0a9d350
Cameron Raw <cameron.raw@me.com>
2016-09-10 09:34:30 +0200

    Added animation to index.php

--- js/app.js
+++ js/app.js
@@ -1,17 +1,33 @@
 $(document).ready(function(){
 	
 	//Load homepage
+	var discoverBanner1 = $("#discover span:nth-child(1)"); //Stores h1
+	var discoverBanner2 = $("#discover span:nth-child(2)");
+	var welcomeBlock = $("#welcome-col");
 
-	var discover = $("#discover span:nth-child(1)");
-	var ofFlawless = $("#discover span:nth-child(2)");
+	welcomeBlock.css({'top':'-20px',
+					'opacity':'0'});
 
 	//Load 'discover' slogan
 
-	
+	function loadPage(){
 
-	//Below code needs re-writing... UGLY
+		discoverBanner1.animate({opacity:1},1000,function(){
+		discoverBanner2.animate({opacity:1},1000, function(){
+			welcomeBlock.animate({opacity:1,bottom: 0}, 1000);
+		});
+		});
 
-	//Then greetings div
+	}
+
+	function loadWelcome(){
+
+		//Then greetings div
+		
+
+	}
+
+	loadPage();
 
 	//Then lucy img
 
